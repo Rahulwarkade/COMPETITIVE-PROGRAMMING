9827779a4dfb72cb3e7a878d38384be2db4a9f54
RAHUL WARKADE <88571832+Rahulwarkade@users.noreply.github.com>
2022-12-21 08:16:45 +0530

    TIME COMPLEXITY ANALYSIS

---
+++ time_complextiy.cpp
@@ -0,0 +1,77 @@
+#include<bit/stdc++.h>
+using namespace std;
+const int N = 1e5;
+int main()
+{
+	int x;
+	int time =0;
+	time = x+x;
+	//Number of iterations =3
+	 int n;
+	 cin>>n; 
+	 for(int i=0; i<n; i++)
+	 {
+	 	int y;
+	 	y=2;
+	 	y--;
+	 	// O(5) + O(3*n) = O(n)
+	 }
+	 /*
+	1e7 = 1second
+	1e8 = 10 second
+	1e9 = 100 second
+	1e10 = 1000 second
+	 */
+	 for(int i=0; i<n; i++)
+	 {
+	 	for(int j=0; j<n; j++)
+	 	{
+	 		// T(n) = O(n^2)
+	 	}
+	 }
+
+	 while(n>0)
+	 {
+	 	n = n/2;
+	 	/*
+	     if n=8
+	     8-4-2-1-1 = O(log(n))
+
+	     2^a = n
+	     log2 = log(n)
+	     alog2 = log(n)
+	     a = log(n)
+		
+		log(1e5) = 15
+
+		T(n) = log(n);
+	 	*/
+	 }
+
+	 for(int i=0; i<n; i++)
+	 {
+	 	for(int j=0; j<i; j++)
+	 	{
+	 		 //T(n) = O(n^2)
+	 	}
+	 }
+
+
+/*
+T = 1e5;
+N = 1e5
+// sum of N over all test cases is <10^7 T(n) = O(n^2)
+but this will still run  because of this constraintsz
+int t;
+cin>>t;
+while(t--)
+{
+	int n;
+	cin>>n;
+	for(int i=0; i<n; i++)
+	{
+		T(n) = O(t); 
+	}
+}
+*/
+}
