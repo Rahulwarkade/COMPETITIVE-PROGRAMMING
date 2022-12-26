c2e0a5e1ed7056ea0e34e059443ae3c533160b0c
RAHUL WARKADE <88571832+Rahulwarkade@users.noreply.github.com>
2022-12-26 20:53:29 +0530

    VERDICTS

---
+++ verdicts.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main()
+{
+
+	string s = (3>1)?"ture":"false";
+	cout<<s<<endl;
+	/*
+	#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;	
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		long long ans =1;
+		for(int i=0; i<n; i++)
+		{
+			int num;
+			cin>>num;
+			ans*=num;
+		}
+	    int least = ans%10;
+		if(least == 5 || least == 3 || least == 2)
+		{
+			cout<<"YES"<<endl;
+		}
+		else
+		{
+			cout<<"NO"<<endl;
+		}
+	}
+}
+	*/
+}
