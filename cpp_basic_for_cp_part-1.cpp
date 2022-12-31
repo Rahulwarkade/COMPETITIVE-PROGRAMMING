b620d5c93d37f8c3fcfb56d3b3f3b75e46dc91b1
RAHUL WARKADE <88571832+Rahulwarkade@users.noreply.github.com>
2022-12-31 21:25:18 +0530

    CPP

---
+++ cpp_basic_for_cp_part-1.cpp
@@ -0,0 +1,154 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int n = 1e7;
+int main()
+{
+	// int a,b;
+	// char c;
+
+	// cin>>a>>b>>c;
+	// cout<<a<<" "<<" "<<b<<" "<<c<<endl;
+	// cout<<(int)c<<endl;
+
+	// bool d = 0;
+
+	// cout<<d<<endl;
+
+	//  a = 9.9;
+	// double z = 9.9;
+
+	// cout<<a<<endl;
+	// cout<<z<<endl;
+
+ //     a = 5;
+ //    cout<<a++<<endl;
+ //    cout<<++a<<endl;
+ //    cout<<a<<endl;
+
+    // char x = 'r';
+    // char a = 'a';
+
+    // cout<<x<<endl;
+    // cout<<(int)x<<endl;
+    // cout<<(int)a<<endl;
+    // cout<<x-a<<endl;
+
+    // int l;
+    // char j;
+    // cin>>j>>l;
+    // cout<<j<<" "<<l<<endl;
+
+
+//  CPP BASIC FOR CP PART -2
+
+	// char,int,float,double,long int,long long
+
+	/*
+    Data type precedence:- double
+                           float
+                           long long int
+                           long int
+                           int
+                           char
+
+    Operator precedence :-    Operators               Associativity
+    						()[]->.++ --  				Left to right
+    						+,-,!,(type)*&sizeof        Right to left
+    						*,/,%                       Left to right      
+	*/
+
+	/*
+	int, char , long int, long long int , float , double, rough ranges
+
+
+	-10^9  <  int  < 10^9
+	-10^12 < long int < 10^12
+	-10^18 < long lont int < 10^18
+
+	*/
+
+   // cout<<7/2.0<<endl;
+
+   // double a = 100000;
+   // double b = 100000;
+   // double c = a*b;
+   // int a = 100000;
+   // int b = 100000;
+   // double c = a*1LL*b;
+   // cout<<(fixed)<<c<<endl;
+
+
+   /*
+   Given rectanle of length l and b. print area of rectangle.
+
+   Constraints:
+   1<= l <= 1e9
+   1<= b <= 1e9 
+
+   Iput Format:
+   2 space sperated integer l an b
+
+   Output Format:
+   Single number which is are of rectanle
+
+   Sample Input:
+   3 5
+
+   Sample Output:
+   15
+
+   */
+
+	// int a;
+	// int b;
+	// cin>>a>>b;
+	// // cout<<a*b; Overflow
+	// cout<<a*1LL*b;
+	
+//    int n,m,o;
+//    cin>>n>>m>>o;
+
+//    int arr[n][m][o];  
+
+// for(int k=0; k<o; k++)
+// {
+
+//    for (int i=0; i<n; i++)
+//    {
+//       for(int j=0; j<m; j++)
+//       {
+//          cin>>arr[i][j][k];
+//       }
+//    }
+// }
+
+// for(int k=0; k<o; k++)
+// {
+//    cout<<k+1<<endl;
+//    for (int i=0; i<n; i++)
+//    {
+//       for(int j=0; j<m; j++)
+//       {
+//          cout<<arr[i][j][k]<<" ";
+//       }
+//       cout<<endl;
+//    }
+
+// }
+
+   // string str;
+   // int t;
+   // cin>>t;
+   // cin.ignore();
+   // while(t--)
+   // {
+   //    getline(cin,str);
+   //    cout<<str<<endl;
+   // }
+   // int n = 1e5;
+   // int arr[n];
+   // arr[n-1] = 24;
+   // cout<<arr[n-1];
+
+   
+}
